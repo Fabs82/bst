b21b62a3215ac50fd9e43aa06cc302a70312c11d
Fabs82 <148846635+Fabs82@users.noreply.github.com>
2026-01-06 15:20:16 +0100

    feat: Implement tree traversal  BFS DFS, delete and find methods
    - delete_node() finds and deletes a node with a given value from the tree
    - find() searches and returns a node with a given value (recursive and iterative both implemented)
    - level_order_iterative() implements a breadth first search BFS traversal using an iterative approach
    - inorder(), preorder(), postorder() implement a depth first search DFS traversal using recursion

--- lib/tree.rb
+++ lib/tree.rb
@@ -10,7 +10,7 @@ class Tree
   end
 
   def build_tree(array, start_point = 0, end_point = array.size - 1)
-    mid_point = (start_point + end_point) / 2
+    mid_point = start_point + (end_point - start_point) / 2
 
     return nil if start_point > end_point
 
@@ -20,17 +20,86 @@ class Tree
     root
   end
 
-  def insert_node(number, root = @root)
-    return root = Node.new(number) if root.nil?
+  def insert_node(number, node = @root)
+    return node = Node.new(number) if node.nil?
 
-    if number < root.value
+    if number < node.value
       # go left
-      root.left = insert_node(number, root.left)
+      node.left = insert_node(number, node.left)
     else
       # go right
-      root.right = insert_node(number, root.right)
+      node.right = insert_node(number, node.right)
     end
-    root
+    node
+  end
+
+  def min_value(node)
+    # helper method to find the min value in the right subtree
+    current = node
+    current = current.left while current.left
+    current.value
+  end
+
+  def delete_node(number, node = @root)
+    return nil if node.nil?
+
+    if number < node.value
+      node.left = delete_node(number, node.left)
+    elsif number > node.value
+      node.right = delete_node(number, node.right)
+    else # three possible case. No child, One child, Two children
+      return node.right if node.left.nil?
+      return node.left if node.right.nil?
+
+      node.value = min_value(node.right)
+      node.right = delete_node(node.value, node.right)
+    end
+    node
+  end
+
+  def insert_iterative(number)
+    return @root = Node.new(number) if @root.nil?
+
+    current = @root
+    while current
+      if number < current.value
+        if current.left.nil?
+          current.left = Node.new(number)
+          break
+        end
+        current = current.left
+      else
+        if current.right.nil?
+          current.right = Node.new(number)
+          break
+        end
+        current = current.right
+      end
+    end
+  end
+
+  def find_recursive(number, node = @root)
+    return nil if node.nil?
+    return node if node.value == number
+
+    if number < node.value
+      find_recursive(number, node.left)
+    else
+      find_recursive(number, node.right)
+    end
+  end
+
+  def find_iterative(number)
+    current = @root
+    while current
+      return current if current.value == number
+
+      current = if number < current.value
+                  current.left
+                else
+                  current.right
+                end
+    end
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
@@ -38,8 +107,73 @@ class Tree
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
+
+  def level_order_iterative
+    # implement a breadth first search BFS traversal using an iterative approach
+    return [] if @root.nil?
+
+    queue = [@root]
+    result = []
+    until queue.empty?
+      current = queue.shift
+      if block_given?
+        yield current
+      else
+        result << current.value
+      end
+      queue << current.left if current.left
+      queue << current.right if current.right
+    end
+    result unless block_given?
+  end
+
+  def inorder(node = @root, result = [], &block)
+    # implement a depth first search DFS traversal using recursion. INORDER <LEFT> <ROOT> <RIGHT>
+    return result if node.nil?
+
+    inorder(node.left, result, &block)
+    yield node if block_given?
+    result << node.value
+    inorder(node.right, result, &block)
+
+    result
+  end
+
+  def preorder(node = @root, result = [], &block)
+    # implement a depth first search DFS traversal using recursion. PREORDER <ROOT> <LEFT> <RIGHT>
+    return result if node.nil?
+
+    yield node if block_given?
+    result << node.value
+    preorder(node.left, result, &block)
+    preorder(node.right, result, &block)
+
+    result
+  end
+
+  def postorder(node = @root, result = [], &block)
+    # implement a depth first search DFS traversal using recursion. POSTORDER <LEFT> <RIGHT> <ROOT>
+    return result if node.nil?
+
+    postorder(node.left, result, &block)
+
+    postorder(node.right, result, &block)
+    yield node if block_given?
+    result << node.value
+
+    result
+  end
 end
 
 tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-tree.insert_node(8000)
+tree.insert_iterative(8000)
+tree.insert_node(12)
 tree.pretty_print
+tree.delete_node(8)
+tree.pretty_print
+
+# yield block test
+# tree.postorder { |node| puts "Visiting node: #{node.value}" }
+p tree.postorder
+p tree.inorder
+p tree.preorder
